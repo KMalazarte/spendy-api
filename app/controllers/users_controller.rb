0abaf6a141a2cc38fcd403153fe97d83993771e9
github username <github email address>
2019-06-27 17:49:11 -0400

    added index to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
+  def index
+    @all = User.all
+    
+  end
+
   def profile
     render json: { user: UserSerializer.new(current_user()) }, status: :accepted
   end
