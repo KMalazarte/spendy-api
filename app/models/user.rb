544211322dd9a636d2d642fd3a623bb94b55d85c
github username <github email address>
2019-07-02 08:52:29 -0400

    follows created for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   has_secure_password
   validates :username, uniqueness: { case_sensitive: false }
   has_many :purchases
+
+  has_many :follows
+  has_many :followees, through: :follows
+  has_many :inverse_follows, class_name: "Follow", foreign_key: 'followee_id'
+  has_many :inverse_followees, through: :inverse_follows, source: :user
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_25_213411) do
+ActiveRecord::Schema.define(version: 2019_07_01_210536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "followee_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "purchases", force: :cascade do |t|
     t.string "date"
