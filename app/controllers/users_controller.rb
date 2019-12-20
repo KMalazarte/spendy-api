1ac638ad52781989c2b7fa3cc35ef8143e1d17ea
Kevin Malazarte <Kevin.Malazarte@gmail.com>
2019-12-20 14:26:05 -0500

    added guard clause to create action on users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,13 +24,21 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    if @user.valid?
-      @token = encode_token(user_id: @user.id)
-      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
-    else
-      render json: { error: 'failed to create user' }, status: :not_acceptable
+    if !@user.valid?
+      return render json: { error: 'failed to create user' }, status: :not_acceptable
     end
+    @token = encode_token(user_id: @user.id)
+    render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
   end
+  # if @user.valid?
+  #   @token = encode_token(user_id: @user.id)
+  #   render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+  # else
+  #   render json: { error: 'failed to create user' }, status: :not_acceptable
+  # end
+
+  # guard_clause - pattern refactor if statements to reduce complexity, test for error case earlier on, when there is ther error return early
+  # one if statement no else clause behavior wont change
 
   private
 
